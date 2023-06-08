01bc5d3693d903a4dc4c1ae99c9e2dd883399957
Pipersenia,Rohan <rohan.pipersenia@rakuten.com>
2023-06-08 18:29:53 +0900

    setup env vars

--- app/routers/post.py
+++ app/routers/post.py
@@ -10,8 +10,6 @@ router = APIRouter(prefix="/posts", tags=["posts"])
 def get_posts(db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user), limit: int=10, skip: int=0, search: Optional[str] = ""):
     # cursor.execute("""SELECT * FROM posts""")
     # posts = cursor.fetchall()
-    print(limit)
-    print(search)
     posts = db.query(models.Post).filter(models.Post.title.contains(search)).limit(limit).offset(skip).all()
     return posts
 
