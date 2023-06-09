be0a647e0fb8071d00a02bbae052c6d31c2fc17b
Pipersenia,Rohan <rohan.pipersenia@rakuten.com>
2023-06-09 12:55:43 +0900

    checking commit on dev

--- app/routers/post.py
+++ app/routers/post.py
@@ -10,6 +10,7 @@ router = APIRouter(prefix="/posts", tags=["posts"])
 def get_posts(db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user), limit: int=10, skip: int=0, search: Optional[str] = ""):
     # cursor.execute("""SELECT * FROM posts""")
     # posts = cursor.fetchall()
+    print(current_user)
     posts = db.query(models.Post).filter(models.Post.title.contains(search)).limit(limit).offset(skip).all()
     return posts
 
